bc6c67cc3e9b879b3fa58596df0464665977cc51
Prasanna Mohanty <mkprasanna@hotmail.com>
2026-03-13 00:12:33 -0700

    First terraform pipeline

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "mkp-terraform-demo-bucket-123456789"
+}
