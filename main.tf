e1fe0fc798af61646e55fea076c1e76c8792b9ab
mkprasanna <mkprasanna@hotmail.com>
2026-03-14 13:38:38 -0700

    Creating EC2 instance using terraform + ansible.

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "mkp-terraform-demo-bucket-123456789"
+resource "aws_instance" "demo" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demo-ansible"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.demo.public_ip
 }
