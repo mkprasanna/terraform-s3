d5c6d19650d6c9d0381e4c562378d7525f97cc3f
mkprasanna <mkprasanna@hotmail.com>
2026-03-15 01:02:39 -0700

    Fixing main.tf to use demo-key.

--- main.tf
+++ main.tf
@@ -12,9 +12,36 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_security_group" "ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "default"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "demo" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.demo.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+}
+
+resource "aws_key_pair" "demo" {
+  key_name   = "demo-key"
+  public_key = file("demo-key.pub")
 }
 
 output "public_ip" {
