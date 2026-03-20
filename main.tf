49cd932a8a344e67318e81dbd834e8ee66e42b73
mkprasanna <mkprasanna@hotmail.com>
2026-03-20 00:06:14 -0700

    Creating terraform-s3 inside terraform-s3.

--- main.tf
+++ main.tf
@@ -41,6 +41,11 @@ resource "aws_instance" "demo" {
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.demo.key_name
   vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
   user_data = <<EOF
 #!/bin/bash
 yum update -y
