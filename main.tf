b2915f7c547f31f88d8a14e756300ecf1e0b5e21
Prasanna Mohanty <mkprasanna@hotmail.com>
2026-03-21 17:11:18 -0700

    Adding wait before ansible run. cloud-init finish wait

--- main.tf
+++ main.tf
@@ -54,6 +54,10 @@ resource "aws_key_pair" "demo" {
   public_key = file("demo-key-2.pub")
 }
 
+output "instance_id" {
+  value = aws_instance.demo.id
+}
+
 output "public_ip" {
   value = aws_instance.demo.public_ip
 }
